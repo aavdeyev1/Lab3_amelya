de3d0946b7b27bff164a26fe248cac8a241b124c
aavdeyev1 <aavdeyev1@eagles.ewu.edu>
2021-10-10 10:32:36 -0700

    3

--- 2dGrid.cu
+++ 2dGrid.cu
@@ -31,40 +31,46 @@ int main()
     block.y = 4;
     grid.x  = ceil( (float)dimx / block.x );
     grid.y  = ceil( (float)dimy / block.y );
+    char somestr[7] = " kernel ";
     
     // Use kernel to fill d_a array
     kernel<<<grid, block>>>( d_a, dimx, dimy );
     cudaMemcpy( h_a, d_a, num_bytes, cudaMemcpyDeviceToHost );
-    printArr(h_a, "kernel ", dimy, dimx);
+    printArr(h_a, somestr, dimy, dimx);
 
     // Use kernel2 to fill d_a array
     cudaMemset( d_a, 0, num_bytes );
     kernel2<<<grid, block>>>( d_a, dimx, dimy );
     cudaMemcpy( h_a, d_a, num_bytes, cudaMemcpyDeviceToHost );
+    somestr = "kernel 2";
     printArr(h_a, "kernel 2", dimy, dimx);
 
     // Use kernel3 to fill d_a array
     cudaMemset( d_a, 0, num_bytes );
     kernel3<<<grid, block>>>( d_a, dimx, dimy );
     cudaMemcpy( h_a, d_a, num_bytes, cudaMemcpyDeviceToHost );
+    somestr = "kernel 3";
     printArr(h_a, "kernel 3", dimy, dimx);
 
     // Use kernel4 to fill d_a array
     cudaMemset( d_a, 0, num_bytes );
     kernel4<<<grid, block>>>( d_a, dimx, dimy );
     cudaMemcpy( h_a, d_a, num_bytes, cudaMemcpyDeviceToHost );
+    somestr = "kernel 4";
     printArr(h_a, "kernel 4", dimy, dimx);
    
     // Use kernel5 to fill d_a array
     cudaMemset( d_a, 0, num_bytes );
     kernel5<<<grid, block>>>( d_a, dimx, dimy );
     cudaMemcpy( h_a, d_a, num_bytes, cudaMemcpyDeviceToHost );
+    somestr = "kernel 5";
     printArr(h_a, "kernel 5", dimy, dimx);
    
     // Use kernel6 to fill d_a array
     cudaMemset( d_a, 0, num_bytes );
     kernel6<<<grid, block>>>( d_a, dimx, dimy );
     cudaMemcpy( h_a, d_a, num_bytes, cudaMemcpyDeviceToHost );
+    somestr = "kernel 6";
     printArr(h_a, "kernel 6", dimy, dimx);
 
     free( h_a );
