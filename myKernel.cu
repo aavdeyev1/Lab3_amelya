29d431783de046eddc80c62a7a82b22a44b7fcd4
aavdeyev1 <aavdeyev1@eagles.ewu.edu>
2021-10-11 00:08:33 -0700

    3

--- myKernel.cu
+++ myKernel.cu
@@ -17,7 +17,7 @@ __global__ void kernel2( int *a, int dimx, int dimy )
     int iy   = blockIdx.y*blockDim.y + threadIdx.y;
     int idx = iy*dimx + ix;
     if (ix < 16)
-        a[idx] = blockIdx.x + blockIdx.y*blockDim.x;
+        a[idx] = blockIdx.x*blockDim.x + blockIdx.y*blockDim.x;
 }
 
 __global__ void kernel3( int *a, int dimx, int dimy )
